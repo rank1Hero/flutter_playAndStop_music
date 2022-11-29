26540e1e212a76df043fbe49582ee005fd950b99
rank1Hero <nihad58@hotmail.com>
2022-11-29 22:40:54 +0100

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() {
+  runApp(const XylophoneApp());
+}
+
+class XylophoneApp extends StatefulWidget {
+  const XylophoneApp({super.key});
+
+  @override
+  State<XylophoneApp> createState() => _XylophoneAppState();
+}
+
+class _XylophoneAppState extends State<XylophoneApp> {
+  final player = AudioPlayer();
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                  ),
+                  onPressed: () {
+                    player.play(AssetSource("Metak.mp3"));
+                  },
+                  child: const Text(
+                      "Play",
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.white
+                    ),
+                  ),
+                ),
+                const SizedBox(height:10),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.blue
+                  ),
+                  onPressed: () {
+                    player.stop();
+                  },
+                  child: const Text(
+                    "Stop",
+                    style: TextStyle(
+                        fontSize: 30,
+                      color: Colors.white
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
